4bdbb1bf1a701951ca6d69643430252537dec200
FSSRepo <go778sgt@gmail.com>
2022-06-06 12:15:35 -0600

    more info

--- java-backend/compile-x32.bat
+++ java-backend/compile-x32.bat
@@ -1,7 +1,8 @@
 @echo off
 @echo Compilando...
-g++ -c wrapper.cpp -o wrapper.o -I"C:\mingw64\include" -I"C:\mingw64\x86_64-w64-mingw32\include" -I"..\src" -I"..\src\bullet" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -m32 -DBUILDING_DLL=1
-g++ -shared wrapper.o -o bin\openpl.dll -L"C:\mingw64\x86_64-w64-mingw32\lib32" -static-libgcc ..\windows\bin\Release\libopenpl.a -m32 -Wl,--output-def,bin\libopenpljni.def,--out-implib,bin\libopenpljni.a,--add-stdcall-alias
+if not exist "bin" mkdir "bin"
+g++ -c wrapper.cpp -o wrapper.o -I"..\src" -I"..\src\bullet" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -m32 -DBUILDING_DLL=1
+g++ -shared wrapper.o -o bin\openpl.dll -static-libgcc ..\windows\bin\Release\libopenpl.a -m32 -Wl,--output-def,bin\libopenpljni.def,--out-implib,bin\libopenpljni.a,--add-stdcall-alias
 @echo Se completo la compilacion. Escribe 'e' para terminar
 @set /p Op=
 if %Op% == "e" goto exit
--- java-backend/compile-x64.bat
+++ java-backend/compile-x64.bat
@@ -1,7 +1,8 @@
 @echo off
 @echo Compilando...
-g++ -c wrapper.cpp -o wrapper.o -I"C:\mingw64\include" -I"C:\mingw64\x86_64-w64-mingw32\include" -I"..\src" -I"..\src\bullet" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -m64 -DBUILDING_DLL=1
-g++ -shared wrapper.o -o bin\openpl64.dll -L"C:\mingw64\x86_64-w64-mingw32\lib" -static-libgcc ..\windows\bin\Release64\libopenpl64.a -m64 -Wl,--output-def,bin\libopenpl64jni.def,--out-implib,bin\libopenpl64jni.a,--add-stdcall-alias
+if not exist "bin" mkdir "bin"
+g++ -c wrapper.cpp -o wrapper.o -I"C:\mingw64\x86_64-w64-mingw32\include" -I"..\src" -I"..\src\bullet" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -m64 -DBUILDING_DLL=1
+g++ -shared wrapper.o -o bin\openpl64.dll -static-libgcc ..\windows\bin\Release64\libopenpl64.a -m64 -Wl,--output-def,bin\libopenpl64jni.def,--out-implib,bin\libopenpl64jni.a,--add-stdcall-alias
 @echo Se completo la compilacion. Escribe 'e' para terminar
 @set /p Op=
 if %Op% == "e" goto exit
