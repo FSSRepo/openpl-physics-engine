158a7d12e88a498e6f8591eee2f5c2ab4e1e3bf8
FSSRepo <go778sgt@gmail.com>
2022-06-06 11:45:35 -0600

    fixed names

--- java-backend/compile-x32.bat
+++ java-backend/compile-x32.bat
@@ -1,6 +1,6 @@
 @echo off
 @echo Compilando...
-g++ -c wrapper.cpp -o wrapper.o -I"C:\mingw64\include" -I"C:\mingw64\x86_64-w64-mingw32\include" -I"..\src" -I"..\src\bullet" -I"C:\Program Files\Java\jdk-11.0.13\include" -I"C:\Program Files\Java\jdk-11.0.13\include\win32" -m32 -DBUILDING_DLL=1
+g++ -c wrapper.cpp -o wrapper.o -I"C:\mingw64\include" -I"C:\mingw64\x86_64-w64-mingw32\include" -I"..\src" -I"..\src\bullet" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -m32 -DBUILDING_DLL=1
 g++ -shared wrapper.o -o bin\openpl.dll -L"C:\mingw64\x86_64-w64-mingw32\lib32" -static-libgcc ..\windows\bin\Release\libopenpl.a -m32 -Wl,--output-def,bin\libopenpljni.def,--out-implib,bin\libopenpljni.a,--add-stdcall-alias
 @echo Se completo la compilacion. Escribe 'e' para terminar
 @set /p Op=
